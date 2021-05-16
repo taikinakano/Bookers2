bcedc3ee59f7bc08fadfb300188ddedffaf8a331
taikinakano <kabayaki3323@gmail.com>
2021-05-16 14:05:29 +0000

    [Update]Rspec test

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,10 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image
   validates :name, presence: true
-  #validates :introduction, presence: true
-  #validates :profile_image, presence: true
+
+  with_options on: :update? do
+   validates :introduction, presence: true
+   validates :profile_image, presence: true
+  end
 
 end
